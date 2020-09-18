417e3a92b36d0f6d4651b7524635ac62ddf3115d
Michaelsliem <31589154+Michaelsliem@users.noreply.github.com>
2020-09-18 12:28:28 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,35 @@
+
 from google.oauth2.service_account import Credentials
 from googleapiclient import discovery
-import gspread
-import pprint
+from google.cloud import bigquery
 
-gc = gspread.service_account(filename='sa.json')
-file = gc.open('quota')
 
-worksheet = file.worksheet(title='quota_limit_usage')
 
 CLIENT_SECRETS_FILE_PATH = "sa.json"
 AUTH_SCOPES = ['https://www.googleapis.com/auth/cloud-platform']
-
+table_id = '<project_id>.<dataset>.<tableid>'
 base_credentials = Credentials.from_service_account_file(CLIENT_SECRETS_FILE_PATH,scopes=AUTH_SCOPES)
+project_id = '<project_id>'
+
+client = bigquery.Client(credentials=base_credentials, project=project_id)
 
 
 service1 = discovery.build('cloudresourcemanager', 'v1', credentials=base_credentials)
-service = discovery.build('compute','v1',credentials=base_credentials)
+service2 = discovery.build('compute','v1',credentials=base_credentials)
+req = service1.projects()
+res = req.list().execute()
+table = client.get_table(table_id)
 
-project = 'ivory-strategy-236322'
-region = 'us-central1'
-
-
-
-request = service.regions().get(project=project, region=region)
-response = request.execute()
-
-pprint.pprint(response)
+for p in res['projects'][0:5]:
+    print(p['projectId'])
+    request = service2.projects().get(project=p['projectId']).execute()
+    for q in request['quotas']:
+        metric = q['metric']
+        usage = q['usage']
+        limit = q['limit']
+        rows_to_insert = [(p['projectId']), (metric), (usage), (limit)]
+        errors = client.insert_rows(table, [rows_to_insert])
+        if errors == []:
+            print("New row have been added")
+        print(metric, usage, limit, errors)
 
